bcb942a3c41ba3fa39b7d6f67cffb6f1110f5907
Erick Maeda <erick.maeda26@gmail.com>
2021-01-22 15:24:43 +0000

    - Initial commit;

---
+++ App.js
@@ -0,0 +1,100 @@
+import React, {useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  StatusBar,
+  Button,
+} from 'react-native';
+import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
+import analytics from '@segment/analytics-react-native';
+import Firebase from '@segment/analytics-react-native-google-analytics';
+
+const App = () => {
+  useEffect(() => {
+    setupAnalytics();
+  }, []);
+
+  const setupAnalytics = () => {
+    analytics
+      .setup('ADD_YOUR_KEY_HERE', {
+        recordScreenViews: true,
+        trackAppLifecycleEvents: true,
+        using: [Firebase],
+        android: {
+          flushInterval: 60000, // 60 seconds
+          collectDeviceId: true,
+        },
+        ios: {
+          trackAdvertising: true,
+          trackDeepLinks: true,
+        },
+      })
+      .then(() => console.log('Analytics is ready'))
+      .catch((err) => console.error('Something went wrong', err));
+  };
+
+  const sendEvent = (eventName, eventParams) => {
+    analytics.track(eventName, eventParams);
+  };
+  const sendLoginEvent = () => {
+    analytics.identify("a user's id", {
+      email: 'jsmith@example.com',
+      name: 'John Smith',
+    });
+  };
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Button title="Event login" onPress={sendLoginEvent} />
+            </View>
+            <View style={styles.sectionContainer}>
+              <Button
+                title="Event navigation"
+                onPress={() =>
+                  sendEvent('navigation', {from: 'login', to: 'register'})
+                }
+              />
+            </View>
+            <View style={styles.sectionContainer}>
+              <Button
+                title="Event step started"
+                onPress={() => sendEvent('step_started', {stepId: '1'})}
+              />
+            </View>
+            <View style={styles.sectionContainer}>
+              <Button
+                title="Event step finished"
+                onPress={() => sendEvent('step_finished', {stepId: '1'})}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # segment-analytics-example
-# segment-analytics-example
+
+## Setup
+
+### Create Segment Project
+- Create a project in [Segment](https://app.segment.com/)
+- Add React Native as a source
+- Create an API Key in `https://app.segment.com/{project}/sources/react_native/settings/keys` (Please replace {project} with your project name)
+
+### Project Setup
+- `yarn`
+- `cd ios && pod install`
+- Enter in `App.js` and replace **ADD_YOUR_KEY_HERE** with your own `writeKey` provided by create segment step
+
+## Run
+- `yarn ios`
+- `yarn android`
+
+
